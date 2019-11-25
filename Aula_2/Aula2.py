ea9a4f33d9b42446506568d9eaffc999944be239
Mateus-Silva11 <alvesdasilva1106@gmail.com>
2019-11-25 08:23:48 -0300

    Aprendendo a Função print(format)

---
+++ Aula_2/Aula2.py
@@ -0,0 +1,26 @@
+#variaveis
+
+idade = 16
+salario = 67770
+nome = "Mateus"
+verdadeiro = True
+falso = False
+
+print("="*50, "\n"*2)
+
+#Print Normal
+print("\t Calypso mania", "Joelma é top")
+print("\t Exalta mania")
+print("\t",'Nome :',nome ,'idade', idade ,'Salario', salario  )
+print("\t",'Verdadeiro',verdadeiro)
+print("\t",'Falso',falso)
+
+#print Format
+
+print('\t','Nome: {} idade: {} Salario: {} Verdadeiro : {} falso: {}'
+.format(nome , idade , salario , verdadeiro , falso))
+
+#printo Terceira opição interpolação de strings
+print('\t',f'Nome: {nome} idade: {idade} Salario: {salario} Verdadeiro : {verdadeiro} falso: {falso}')
+
+print("\n"*2,'='*50)
