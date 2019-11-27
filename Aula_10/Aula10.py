369399d5d06020a066e4ffeab2be48e11ce5f3c2
Mateus-Silva11 <alvesdasilva1106@gmail.com>
2019-11-27 08:52:46 -0300

    Web metodos return e html

---
+++ Aula_10/Aula10.py
@@ -0,0 +1,37 @@
+#wed - Calculadora
+
+nome_pagina = 'Calculadora'
+
+
+from flask import Flask , render_template , request
+from operacaoes3 import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html',titulo=nome_pagina)
+
+@app.route('/calcular')
+def calcular():
+    n1 = int(request.args['n1']) 
+    n2= int(request.args['n2'])
+    
+    r_soma = soma(n1,n2)
+    r_subtracao = subtracao(n1,n2)
+    r_multiplicacao = multiplicacao(n1,n2)
+    r_divisao_inteira = divisao_inteira(n1,n2)
+    r_divisao_fracionada = divisao_fracionada(n1,n2)
+    r_resto = resto(n1,n2)
+    r_raiz = raiz(n1,n2)
+
+    Resultados = {'soma':r_soma , 'subtracao':r_subtracao , 'multiplicacao':r_multiplicacao , 'divisao_inteira': r_divisao_inteira,
+    'divisao_fracionada': r_divisao_fracionada , 'resto': r_resto , 'raiz':r_raiz
+    }
+    return render_template('resultado.html' , Resultados = Resultados , n1=n1 , n2=n2)
+    
+    # return render_template('resultado.html', n1=n1 , n2=n2 , soma=r_soma , subtracao= r_subtracao ,
+    #  multiplicacao= r_multiplicacao , divisao_inteira= r_divisao_inteira , divisao_fracionada=r_divisao_fracionada,
+    #  resto=r_resto   , raiz=r_raiz ) 
+
+app.run(host='192.168.1.58', port =8080)
---
+++ Aula_10/operacaoes3.py
@@ -0,0 +1,29 @@
+def soma(n1,n2):
+    r = n1 + n2
+    return r
+
+def subtracao(n1,n2):
+    r= n1 - n2
+    return r
+
+def multiplicacao(n1,n2):
+    r = n1 * n2
+    return r
+
+def divisao_inteira(n1,n2):
+    r = n1 // n2
+    return r
+
+def divisao_fracionada(n1,n2):
+    r = n1 / n2
+    return r
+
+def resto(n1,n2):
+    r = n1 % n2
+    return r
+
+def raiz(n1,n2):    
+    r =  n1 **(1/n2)
+    return r
+
+          
